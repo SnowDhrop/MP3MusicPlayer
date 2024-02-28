68a19e4d37f39a5a171b19acf29707e517f1bed3
SnowDhrop <nath.arditti@yahoo.fr>
2024-02-28 15:48:00 +0100

    feat: initialize GUI

---
+++ swing/MP3MusicPlayer/src/MusicPlayerGUI.java
@@ -0,0 +1,17 @@
+package src;
+
+import javax.swing.*;
+
+public class MusicPlayerGUI extends JFrame {
+    public MusicPlayerGUI() {
+        super("Music Player");
+        setSize(400, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Launch the app at the center of the screen
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setLayout(null);
+    }
+}
