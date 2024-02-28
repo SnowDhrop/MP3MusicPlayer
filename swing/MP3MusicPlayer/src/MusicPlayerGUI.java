432d00a141c337babd7508e0c6365bd5b409b00d
SnowDhrop <nath.arditti@yahoo.fr>
2024-02-28 17:16:20 +0100

    feat: add GUI

--- swing/MP3MusicPlayer/src/MusicPlayerGUI.java
+++ swing/MP3MusicPlayer/src/MusicPlayerGUI.java
@@ -1,8 +1,16 @@
 package src;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class MusicPlayerGUI extends JFrame {
+    // Color configurations
+    public static final Color FRAME_COLOR = Color.BLACK;
+    public static final Color TEXT_COLOR = Color.WHITE;
+
     public MusicPlayerGUI() {
         super("Music Player");
         setSize(400, 600);
@@ -12,6 +20,154 @@ public class MusicPlayerGUI extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
 
+        // Set layout to null which allows me to control (x, y, height, width) of my components
         setLayout(null);
+
+        // Change the frame color
+        getContentPane().setBackground(FRAME_COLOR);
+        
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        addToolbar();
+
+        // Icone
+        JLabel songImage = new JLabel(loadImage("src/assets/record.png"));
+        songImage.setBounds(0, 50, getWidth() - 20, 225);
+        add(songImage);
+
+        // Song title
+        JLabel songTitle = new JLabel("Song Title");
+        songTitle.setBounds(0, 285, getWidth() - 10, 30);
+        songTitle.setFont(new Font("Dialog", Font.BOLD, 24));
+        songTitle.setForeground(TEXT_COLOR);
+        songTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songTitle);
+
+        // Song artist
+        JLabel songArtist = new JLabel("Artist");
+        songArtist.setBounds(0, 315, getWidth() - 10, 30);
+        songArtist.setFont(new Font("Dialog", Font.PLAIN, 24));
+        songArtist.setForeground(TEXT_COLOR);
+        songArtist.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songArtist);
+
+        // Playback slider
+        JSlider playbackSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+        playbackSlider.setBounds(getWidth() / 2 - 300/2, 365, 300, 40);
+        playbackSlider.setBackground(null);
+        add(playbackSlider);
+
+        // Playback buttons
+        addPlaybackBtns();
+    }
+
+    private void addPlaybackBtns() {
+        JPanel playbackBtns = new JPanel();
+        playbackBtns.setBounds(0, 435, getWidth() - 10, 80);
+        playbackBtns.setBackground(null);
+
+        // Previous button
+        JButton prevButton = new JButton(loadImage("src/assets/previous.png"));
+        prevButton.setBorderPainted(false);
+        prevButton.setBackground(null);
+        playbackBtns.add(prevButton);
+
+        // Play Button
+        JButton playButton = new JButton(loadImage("src/assets/play.png"));
+        playButton.setBorderPainted(false);
+        playButton.setBackground(null);
+        playbackBtns.add(playButton);
+
+        // Pause Button
+        JButton pauseButton = new JButton(loadImage("src/assets/pause.png"));
+        pauseButton.setBorderPainted(false);
+        pauseButton.setBackground(null);
+        pauseButton.setVisible(false);
+        playbackBtns.add(pauseButton);
+
+        // Next Button
+        JButton nextButton = new JButton(loadImage("src/assets/next.png"));
+        nextButton.setBorderPainted(false);
+        nextButton.setBackground(null);
+        playbackBtns.add(nextButton);
+
+        add(playbackBtns);
+    }
+
+    private void addToolbar() {
+        // Toolbar
+        JToolBar toolBar = new JToolBar();
+        toolBar.setBounds(0, 0, getWidth(), 20);
+        // Prevent toolbar from being moved
+        toolBar.setFloatable(false);
+
+        // Dropdown Menu
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        // Song menu
+        JMenu songMenu = new JMenu("Song");
+        menuBar.add(songMenu);
+
+        // Load song item in the song menu
+        JMenuItem loadSong = new JMenuItem("Load Song");
+        songMenu.add(loadSong);
+
+        // Playlist menu
+        JMenu playlistMenu = new JMenu("Playlist");
+        menuBar.add(playlistMenu);
+
+        // Create playlist into playlist menu
+        JMenuItem createPlaylist = new JMenuItem("Create Playlist");
+        playlistMenu.add(createPlaylist);
+
+        // Load playlist into playlist menu
+        JMenuItem loadPlaylist = new JMenuItem("Load Playlist");
+        playlistMenu.add(loadPlaylist);
+
+        add(toolBar);
+    }
+
+    private ImageIcon loadImage(String imagePath) {
+        try {
+            // Read image file from the give path
+            BufferedImage image = ImageIO.read(new File(imagePath));
+            return new ImageIcon(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
